112a31b9e2e49ba1cc65e0e22ca5fcf13375e092
Ben Hamill <git-commits@benhamill.com>
2010-04-10 13:34:12 -0500

    Working.

--- lib/twitter_alert.rb
+++ lib/twitter_alert.rb
@@ -1,2 +1,4 @@
+require 'grackle'
+
 require 'twitter_alert/alert'
 require 'twitter_alert/account'
--- lib/twitter_alert/account.rb
+++ lib/twitter_alert/account.rb
@@ -1,41 +1,40 @@
-require 'rubygems'
-require 'grackle'
+module TwitterAlert
+  class Account
+    def initialize config
+      # Load hash from yaml file in default location?
 
-class Account
-  def initialize config
-    # Load hash from yaml file in default location?
+      @username = config[:user_name]
+      @password = config[:password]
 
-    @username = config[:user_name]
-    @password = config[:password]
-
-    begin
-      @client = Grackle::Client.new(
-        :auth => {
-          :type => :basic,
-          :username => @username,
-          :password => @password
-        }#,
-#       :ssl => true
-      )
-    rescue Grackle::TwitterError, e
-      #put something here, I'm sure.
-    end
-  end
-
-  def announce message
-    followers.each do |follower|
       begin
-        @client.direct_messages.new! :user_id => follower, :text => message.text
+        @client = Grackle::Client.new(
+          :auth => {
+            :type => :basic,
+            :username => @username,
+            :password => @password
+          }#,
+  #       :ssl => true
+        )
       rescue Grackle::TwitterError, e
-        @failed_announcements ||= []
-        @failed_announcements << follower
+        #put something here, I'm sure.
       end
     end
 
-    message.mark_sent
-  end
+    def announce message
+      followers.each do |follower|
+        begin
+          @client.direct_messages.new! :user_id => follower, :text => message.text
+        rescue Grackle::TwitterError, e
+          @failed_announcements ||= []
+          @failed_announcements << follower
+        end
+      end
 
-  def followers
-    @client.followers.ids? :screen_name => @user_name
+      message.mark_sent
+    end
+
+    def followers
+      @client.followers.ids? :screen_name => @user_name
+    end
   end
 end
