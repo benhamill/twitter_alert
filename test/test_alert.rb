6a182be94ba88839ed990a2887393a889bb6d507
Ben Hamill <git-commits@benhamill.com>
2010-04-21 17:43:42 -0500

    Tests for all basic Alert functionality.

--- test/test_alert.rb
+++ test/test_alert.rb
@@ -5,10 +5,21 @@ class TestAlert < Test::Unit::TestCase
     include TwitterAlert::Alert
   end
 
-  def test_new
-    alert = AlertTester.new 'Test alert.', '01/02/2010'
+  def setup
+    @alert = AlertTester.new 'Test alert.', '01/02/2010'
+  end
 
-    assert_equal('Test alert.', alert.text)
-    assert_equal(DateTime.parse('01/02/2010'), alert.date)
+  def test_new
+    assert_equal('Test alert.', @alert.text)
+    assert_equal(DateTime.parse('01/02/2010'), @alert.date)
+  end
+
+  def test_new_alerts_are_marked_unsent
+    assert(!@alert.sent?, "New Alerts shouldn't be marked sent.")
+  end
+
+  def test_alerts_can_be_marked_sent
+    @alert.mark_sent
+    assert(@alert.sent?, "Alerts should be able to be marked sent.")
   end
 end
