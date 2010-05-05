dcf241bd2f39a93585be5b926d02fa2253dbd2e3
Ben Hamill <git-commits@benhamill.com>
2010-05-05 17:42:13 -0500

    Simplify test. Add red test.

--- test/test_alert.rb
+++ test/test_alert.rb
@@ -20,26 +20,14 @@ class TestAlert < Test::Unit::TestCase
   end
 
   def test_failed_announcements
-    @account = TwitterAlert::Account.new :user_name => 'test_user', :password => 'test_password'
-
-    FakeWeb.allow_net_connect = false
-    FakeWeb.clean_registry
-
-    FakeWeb.register_uri :get, 'http://test_user:test_password@twitter.com/followers/ids.json', :body => '[1, 2, 3]'
-    FakeWeb.register_uri :post, 'http://test_user:test_password@twitter.com/direct_messages/new.json',
-      [{
-         :body => '{"sender_id":12345678,"recipient_id":87654321,"text":"Test message."}',
-         :status => ['200', 'OK']
-       }, {
-         :body => '{"request":"/direct_messages/new.json","error":"Not found"}',
-         :status => ['404', 'Not found']
-       }, {
-         :body => '{"sender_id":12345678,"recipient_id":87654321,"text":"Test message."}',
-         :status => ['200', 'OK']
-       }]
-
-    @account.announce(@alert)
-
+    @alert.add_failed_announcement 2, 'post http://twitter.com/direct_messages/new.json => 404: {"request":"/direct_messages/new.json","error":"Not found"}'
     assert_equal([{:follower_id => 2, :error_text => 'post http://twitter.com/direct_messages/new.json => 404: {"request":"/direct_messages/new.json","error":"Not found"}'}], @alert.failed_announcements)
+  end
+
+  def test_having_failed_announcements_blocks_being_marked_sent
+    @alert.add_failed_announcement 2, 'post http://twitter.com/direct_messages/new.json => 404: {"request":"/direct_messages/new.json","error":"Not found"}'
+    
+    @alert.mark_sent
+    assert(!@alert.sent?, "Alerts with failed announcements should not be able to be marked sent.")
   end
 end
